6b733bd631d6cffe5e20a4168627069ab1d9882f
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-26 13:26:48 +0200

    fix skills

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name: str):
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill):
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"]
 
     def __init__(self, name: str):
         super().__init__(name)
+        self.learn_skill("JavaScript")
+        self.learn_skill("HTML")
+        self.learn_skill("CSS")
 
     def create_awesome_web_page(self):
         print(f"{self.name} is creating a webpage...")
@@ -20,10 +22,12 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"]
 
     def __init__(self, name: str):
         super().__init__(name)
+        self.learn_skill("Python")
+        self.learn_skill("SQL")
+        self.learn_skill("Django")
 
     def create_powerful_api(self):
         print(f"{self.name} is creating an API...")
@@ -31,10 +35,11 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"]
 
     def __init__(self, name: str):
         super().__init__(name)
+        self.learn_skill("Java")
+        self.learn_skill("Android studio")
 
     def create_smooth_mobile_app(self):
         print(f"{self.name} is creating a mobile app...")
@@ -42,16 +47,11 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    skills = [
-        "Python",
-        "SQL",
-        "Django",
-        "JavaScript",
-        "HTML",
-        "CSS",
-    ]
+
+    def __init__(self, name):
+        super().__init__(name)
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
-        super(FullStackDeveloper, self).create_powerful_api()
-        super(FullStackDeveloper, self).create_awesome_web_page()
+        self.create_powerful_api()
+        self.create_awesome_web_page()
