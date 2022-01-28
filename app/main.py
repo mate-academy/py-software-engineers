839233ddbb36afe5d50b610a1e7b7fac22d6b93e
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-01-28 14:52:57 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
 class SoftwareEngineer:
-    skills = []
-
     def __init__(self, name):
         self.name = name
+        self.skills = []
 
-    @classmethod
-    def learn_skill(cls, skill):
-        cls.skills.append(skill)
+    def learn_skill(self, skill):
+        self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = [
-        "JavaScript",
-        "HTML",
-        "CSS"
-    ]
-
     def __init__(self, name):
         super(FrontendDeveloper, self).__init__(name)
+        self.skills = [
+            "JavaScript",
+            "HTML",
+            "CSS"
+        ]
 
     def create_awesome_web_page(self):
         print(f"{self.name} is creating a webpage...")
@@ -26,14 +23,13 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = [
-        "Python",
-        "SQL",
-        "Django"
-    ]
-
     def __init__(self, name):
         super(BackendDeveloper, self).__init__(name)
+        self.skills = [
+            "Python",
+            "SQL",
+            "Django"
+        ]
 
     def create_powerful_api(self):
         print(f"{self.name} is creating an API...")
@@ -42,13 +38,12 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = [
-        "Java",
-        "Android studio"
-    ]
-
     def __init__(self, name):
         super(AndroidDeveloper, self).__init__(name)
+        self.skills = [
+            "Java",
+            "Android studio"
+        ]
 
     def create_smooth_mobile_app(self):
         print(f"{self.name} is creating a mobile app...")
@@ -57,17 +52,16 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    skills = [
-        "Python",
-        "SQL",
-        "Django",
-        "JavaScript",
-        "HTML",
-        "CSS"
-    ]
-
     def __init__(self, name):
         super(FullStackDeveloper, self).__init__(name)
+        self.skills = [
+            "Python",
+            "SQL",
+            "Django",
+            "JavaScript",
+            "HTML",
+            "CSS"
+        ]
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
