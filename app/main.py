30a400eb100e34f6e86eddf1c413e6a24e43dd02
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-01-29 16:00:49 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -10,11 +10,13 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name):
         super(FrontendDeveloper, self).__init__(name)
-        self.skills = [
-            "JavaScript",
-            "HTML",
-            "CSS"
-        ]
+        self.skills.extend(
+            [
+                "JavaScript",
+                "HTML",
+                "CSS"
+            ]
+        )
 
     def create_awesome_web_page(self):
         print(f"{self.name} is creating a webpage...")
@@ -25,11 +27,13 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name):
         super(BackendDeveloper, self).__init__(name)
-        self.skills = [
-            "Python",
-            "SQL",
-            "Django"
-        ]
+        self.skills.extend(
+            [
+                "Python",
+                "SQL",
+                "Django"
+            ]
+        )
 
     def create_powerful_api(self):
         print(f"{self.name} is creating an API...")
@@ -40,10 +44,12 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name):
         super(AndroidDeveloper, self).__init__(name)
-        self.skills = [
-            "Java",
-            "Android studio"
-        ]
+        self.skills.extend(
+            [
+                "Java",
+                "Android studio"
+            ]
+        )
 
     def create_smooth_mobile_app(self):
         print(f"{self.name} is creating a mobile app...")
@@ -54,14 +60,6 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name):
         super(FullStackDeveloper, self).__init__(name)
-        self.skills = [
-            "Python",
-            "SQL",
-            "Django",
-            "JavaScript",
-            "HTML",
-            "CSS"
-        ]
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
