e7abada4bc1cb387d0ec9d6356e2c4ee0aa11ab5
Dasha Protsko <protscodasha@icloud.com>
2022-05-29 21:21:07 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -38,8 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str):
-        super().__init__(name)
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
