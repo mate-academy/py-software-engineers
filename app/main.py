7b8741c6f9f6745be55f1ff3699e16808cb27715
Andrei Korotenko <82520549+Splend1ed@users.noreply.github.com>
2022-06-12 22:28:09 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -55,5 +55,3 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
-# print(FullStackDeveloper.__mro__)
