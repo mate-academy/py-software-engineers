e4e5d8fa22d6ea45af32f412d1099d596ded18b3
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-13 14:27:36 +0300

    changed attr skills

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name):
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill):
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"]
 
     def __init__(self, name):
         super().__init__(name)
-        self.name = name
+        self.skills += ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self):
         print(f"{self.name} is creating a webpage...")
@@ -21,11 +20,10 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"]
 
     def __init__(self, name):
         super().__init__(name)
-        self.name = name
+        self.skills += ["Python", "SQL", "Django"]
 
     def create_powerful_api(self):
         print(f"{self.name} is creating an API...")
@@ -33,11 +31,10 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"]
 
     def __init__(self, name):
         super().__init__(name)
-        self.name = name
+        self.skills = ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self):
         print(f"{self.name} is creating a mobile app...")
@@ -48,12 +45,8 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
     def __init__(self, name):
         super().__init__(name)
-        self.name = name
-        self.skills = BackendDeveloper.skills + FrontendDeveloper.skills
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
-# print(FullStackDeveloper.__mro__)
