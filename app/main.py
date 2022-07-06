3e37f967bbe12391cade7dbf4ec04aab2193a7c7
Misha Hrynko <misha@mate.academy>
2022-07-06 16:44:00 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -44,10 +44,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
-    def __init__(self, name):
-        super().__init__(name)
-        self.skills = self.skills
-
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
