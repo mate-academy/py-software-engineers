35542221313fd3393f4713bf0bc9b25aa945acea
KevinXD <kevinfdevid@gmail.com>
2022-08-16 13:47:42 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name):
         self.name = name
+        self.skills = []
 
-    @classmethod
-    def learn_skill(cls, skill):
-        cls.skills.append(skill)
+    def learn_skill(self, skill):
+        self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = [
-        "JavaScript",
-        "HTML",
-        "CSS"
-    ]
 
     def __init__(self, name):
         super().__init__(name)
+        self.skills += [
+            "JavaScript",
+            "HTML",
+            "CSS"
+        ]
 
     def create_awesome_web_page(self):
         print(f"{self.name} is creating a webpage...")
@@ -25,14 +24,14 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = [
-        "Python",
-        "SQL",
-        "Django",
-    ]
 
     def __init__(self, name):
         super().__init__(name)
+        self.skills += [
+            "Python",
+            "SQL",
+            "Django",
+        ]
 
     def create_powerful_api(self):
         print(f"{self.name} is creating an API...")
@@ -40,29 +39,22 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = [
-        "Java",
-        "Android studio",
-    ]
 
     def __init__(self, name):
         super().__init__(name)
+        self.skills += [
+            "Java",
+            "Android studio",
+        ]
 
     def create_smooth_mobile_app(self):
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper,
-                         FrontendDeveloper,
+class FullStackDeveloper(FrontendDeveloper,
+                         BackendDeveloper,
                          SoftwareEngineer):
-    skills = [
-        "Python",
-        "SQL",
-        "Django",
-        "JavaScript",
-        "HTML",
-        "CSS", ]
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
