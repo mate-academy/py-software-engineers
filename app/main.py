b138c7a2123beadcc91c6c528d4da4c48f8c9f81
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-08-18 20:10:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,42 @@
-# write your code here
+class SoftwareEngineer:
+    skills = []
+
+    def __init__(self, name: str):
+        self.name = name
+
+    @classmethod
+    def learn_skill(cls, skill: str):
+        cls.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    skills = ['JavaScript', 'HTML', 'CSS']
+
+    def create_awesome_web_page(self):
+        print(f'{self.name} is creating a webpage...')
+        return '<h1>Hello world</h1>'
+
+
+class BackendDeveloper(SoftwareEngineer):
+    skills = ['Python', 'SQL', 'Django']
+
+    def create_powerful_api(self):
+        print(f'{self.name} is creating an API...')
+        return 'http://127.0.0.1:8000'
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    skills = ['Java', 'Android studio']
+
+    def create_smooth_mobile_app(self):
+        print(f'{self.name} is creating a mobile app...')
+        return 'Ads every three swipes'
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    skills = FrontendDeveloper.skills + BackendDeveloper.skills
+
+    def create_web_application(self):
+        print(f'{self.name} started creating a web application...')
+        self.create_powerful_api()
+        self.create_awesome_web_page()
