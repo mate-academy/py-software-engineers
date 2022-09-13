8a36fd5ecd9475de913968991c3f89e8049c1d73
Diana <daria.blessed18@gmail.com>
2022-09-13 21:35:57 +0300

    version three

--- app/main.py
+++ app/main.py
@@ -1,45 +1,47 @@
 class SoftwareEngineer:
 
-    skills = []
-
     def __init__(self, name):
         self.name = name
+        self.skills = []
 
-    @classmethod
-    def learn_skill(cls, skill):
-        cls.skills.append(skill)
-
-
-class FrontendDeveloper(SoftwareEngineer):
-
-    skills = ["JavaScript", "HTML", "CSS"]
-
-    def create_awesome_web_page(self):
-        print(f"{self.name} is creating a webpage...")
-        return "<h1>Hello world</h1>"
+    def learn_skill(self, skill):
+        self.skills.append(skill)
 
 
 class BackendDeveloper(SoftwareEngineer):
 
-    skills = ["Python", "SQL", "Django"]
+    def __init__(self, name):
+        super().__init__(name)
+        self.skills += ["Python", "SQL", "Django"]
 
     def create_powerful_api(self):
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
+class FrontendDeveloper(SoftwareEngineer):
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.skills += ["JavaScript", "HTML", "CSS"]
+
+    def create_awesome_web_page(self):
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
 class AndroidDeveloper(SoftwareEngineer):
 
-    skills = ["Java", "Android studio"]
+    def __init__(self, name):
+        super().__init__(name)
+        self.skills += ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self):
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-
-    skills = FrontendDeveloper.skills + BackendDeveloper.skills
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
