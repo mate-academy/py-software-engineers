40e2511a73298bdb88a6d52f556f19916aa364b3
Yulia Bozhieva <bozhieva93@gmail.com>
2022-09-19 09:56:24 -0400

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name):
         self.name = name
+        self.skills = []
 
-    @classmethod
-    def learn_skill(cls, skill):
-        print("skill", skill)
-        cls.skills.append(skill)
+    def learn_skill(self, skill):
+        self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"]
 
     def __init__(self, name):
         super().__init__(name)
+        self.skills = ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self):
         print(f"{self.name} is creating a webpage...")
@@ -22,10 +20,10 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"]
 
     def __init__(self, name):
         super().__init__(name)
+        self.skills = ["Python", "SQL", "Django"]
 
     def create_powerful_api(self):
         print(f"{self.name} is creating an API...")
@@ -33,10 +31,10 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"]
 
     def __init__(self, name):
         super().__init__(name)
+        self.skills = ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self):
         print(f"{self.name} is creating a mobile app...")
@@ -44,10 +42,10 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
 
     def __init__(self, name):
         super().__init__(name)
+        self.skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
 
     def create_web_application(self):
         print(f"{self.name} started creating a web application...")
