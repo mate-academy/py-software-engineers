11f87cf74c2fec3f1729537af69c4f50e11f20de
Kateryna Kilyk <111740601+kateryna3k@users.noreply.github.com>
2022-09-21 18:55:17 +0300

    Added annotation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class SoftwareEngineer:
     skills = []
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
 
     def learn_skill(self, skill: str) -> None:
