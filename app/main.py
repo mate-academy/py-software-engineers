04e6b3fdcad87a4d34749c480cb9a257d919594f
Edik Marushchak <e.moscow138@gmail.com>
2022-09-22 10:48:02 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,16 +3,18 @@ class SoftwareEngineer:
         self.name = name
         self.skills = []
 
-    def learn_skill(self, skill: int):
+    def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str):
         super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS"]
+        self.skills.extend(
+            ["JavaScript", "HTML", "CSS"]
+        )
 
-    def create_awesome_web_page(self):
+    def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
@@ -20,32 +22,30 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str):
         super().__init__(name)
-        self.skills = ["Python", "SQL", "Django"]
+        self.skills.extend(
+            ["Python", "SQL", "Django"]
+        )
 
-    def create_powerful_api(self):
-        print(f"{self.name} is creating a mobile app...")
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    def __init__(self, name):
+    def __init__(self, name: str):
         super().__init__(name)
-        self.skills = ["Java", "Android studio"]
+        self.skills.extend(
+            ["Java", "Android studio"]
+        )
 
-    def create_smooth_mobile_app(self):
+    def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str):
-        super().__init__(name)
-        self.skills = [
-            "Python", "SQL", "Django",
-            "JavaScript", "HTML", "CSS",
-        ]
-
-    def create_web_application(self):
+class FullStackDeveloper(FrontendDeveloper,
+                         BackendDeveloper):
+    def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
