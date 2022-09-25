82d01bc160b417e963c6024f73cbe79d260a4e01
Alexander <alexzubkoff123@gmail.com>
2022-09-25 22:08:19 +0300

    Cnanged attr skills from class to instance

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name: str) -> None:
         self.name = name
+        self.skills = []
 
-    @classmethod
     def learn_skill(cls, skill: str) -> None:
         cls.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = [
-        "JavaScript",
-        "HTML",
-        "CSS",
-    ]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills = [
+            "JavaScript",
+            "HTML",
+            "CSS",
+        ]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -25,14 +24,14 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = [
-        "Python",
-        "SQL",
-        "Django",
-    ]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills = [
+            "Python",
+            "SQL",
+            "Django",
+        ]
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -40,13 +39,13 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = [
-        "Java",
-        "Android studio",
-    ]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills = [
+            "Java",
+            "Android studio",
+        ]
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -55,7 +54,16 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
-    skills = BackendDeveloper.skills + FrontendDeveloper.skills
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills = [
+            "Python",
+            "SQL",
+            "Django",
+            "JavaScript",
+            "HTML",
+            "CSS",
+        ]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
