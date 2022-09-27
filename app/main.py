61322e3b101b0e0784b5129ba7d558068a65c5c0
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-09-27 10:01:41 +0300

    remove pass from FullStackDeveloper

--- app/main.py
+++ app/main.py
@@ -42,7 +42,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    pass
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
