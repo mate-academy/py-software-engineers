cb4e24f9b5bdba12a578d07a9a09a29e485a5b01
VitaliiPiaskivskyi <Vitaliipiaskivskyi@gmail.com>
2022-09-27 18:41:06 +0300

    27_09

--- app/main.py
+++ app/main.py
@@ -41,5 +41,5 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-        BackendDeveloper.create_powerful_api(self)
-        FrontendDeveloper.create_awesome_web_page(self)
+        self.create_powerful_api()
+        self.create_awesome_web_page()
