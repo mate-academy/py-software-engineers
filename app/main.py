ea2dc61c58cc60d651b5265cf7bd5a1d82ae4ac3
Iurii Lobunets <iurii@lobunets.com.ua>
2022-10-09 17:35:28 +0300

    Solution of py-delivery-drone

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+from __future__ import annotations
+
+if __name__ == "__main__":
+    from developers import SoftwareEngineer, FrontendDeveloper, \
+        BackendDeveloper, AndroidDeveloper, FullStackDeveloper
+else:
+    from .developers import SoftwareEngineer, FrontendDeveloper, \
+        BackendDeveloper, AndroidDeveloper, FullStackDeveloper
+
+
+# Code test block
+if __name__ == "__main__":
+    # Test
+    SoftwareEngineer("Max")
+    FrontendDeveloper("Alisa")
+    BackendDeveloper("Bob")
+    AndroidDeveloper("Beth")
+
+    full_stack_dev = FullStackDeveloper("Tom")
+    full_stack_dev.create_web_application()
