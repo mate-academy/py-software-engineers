c063121a904e5729bdca3881112ea0d1e709b75b
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-23 12:02:50 +0300

    create class Software and class Frontend

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+class SoftwareEngineer:
+    skills = []
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def learn_skill(self, skill: str) -> None:
+        SoftwareEngineer.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str,
+                 skills:
+                 list=["JavaScript", "HTML", "CSS"]) -> None:
+        super().__init__(name)
+        self.skills = skills
