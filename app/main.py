ba6eb9df28de0685844fa027895577e407eaf131
Admin <winofkill@gmail.com>
2022-11-09 15:19:28 +0400

    Done43

--- app/main.py
+++ app/main.py
@@ -49,4 +49,3 @@ class FullStackDeveloper(FrontendDeveloper,
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
