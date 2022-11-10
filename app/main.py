1bea2bd5225a92d182895e3a7537beadfed1ba60
Tina_Bushuieva <tinabushuieva@gmail.com>
2022-11-10 15:19:03 +0100

    removed extra declarations

--- app/main.py
+++ app/main.py
@@ -14,9 +14,6 @@ class FrontendDeveloper(SoftwareEngineer):
 
     skills = ["JavaScript", "HTML", "CSS"]
 
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
@@ -25,9 +22,6 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
 
     skills = ["Python", "SQL", "Django"]
-
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -38,9 +32,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
     skills = ["Java", "Android studio"]
 
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
@@ -49,9 +40,6 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
     skills = BackendDeveloper.skills + FrontendDeveloper.skills
-
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
