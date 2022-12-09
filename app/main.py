f221631b33ae6aa423d0367add6e9944cd633a9d
Kostyantyn Chernyk <chernykko@gmail.com>
2022-12-09 15:21:42 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 class SoftwareEngineer:
-    skills = []
-
     def __init__(self, name: str) -> None:
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"]
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.append("JavaScript")
+        self.skills.append("HTML")
+        self.skills.append("CSS")
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -20,10 +20,11 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"]
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.append("Python")
+        self.skills.append("SQL")
+        self.skills.append("Django")
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -31,22 +32,17 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"]
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.append("Java")
+        self.skills.append("Android studio")
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    skills = BackendDeveloper.skills + FrontendDeveloper.skills
-
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
