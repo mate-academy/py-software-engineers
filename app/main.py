a363ad3bac58c296f17983b4368e38aca6b921d5
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-11 15:13:02 +0300

    'fix'

--- app/main.py
+++ app/main.py
@@ -41,10 +41,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super(SoftwareEngineer, self).__init__(name)
-        self.skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
