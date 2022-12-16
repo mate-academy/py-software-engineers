6ece8992b753ea983d710a645ace836e0c9a559b
Vladimir Titarenko <tytarenkova@gmail.com>
2022-12-17 02:55:04 +0300

    the method __add__ was added

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class SoftwareEngineer:
     skills = []
 
@@ -7,6 +10,10 @@ class SoftwareEngineer:
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
+
+    def __add__(self, other: BackendDeveloper) -> BackendDeveloper:
+        self.skills += other.skills
+        return self
 
 
 class FrontendDeveloper(SoftwareEngineer):
@@ -26,7 +33,7 @@ class BackendDeveloper(SoftwareEngineer):
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python", "SQL", "Django"]
+        self.__add__(BackendDeveloper)
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -46,10 +53,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = BackendDeveloper.skills + FrontendDeveloper.skills
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
