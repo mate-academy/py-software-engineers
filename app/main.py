764b3fb9a947945f34a2f57c8cd4de8dbdd2c26b
Vasyl Borysiuk <jomajok7777@gmail.com>
2023-01-29 01:01:59 +0200

    New right solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name: str) -> None:
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
-        self.skills.append(skill)
+        if skill not in self.skills:
+            self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -20,10 +21,10 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -31,18 +32,17 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    skills = BackendDeveloper.skills + FrontendDeveloper.skills
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
