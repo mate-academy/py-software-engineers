a326629c63096f0cd132933254ee4e7363ef3e0e
oithegod <serejathegod@gmail.com>
2023-02-13 20:12:06 +0200

    identation fix

--- app/main.py
+++ app/main.py
@@ -41,7 +41,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
