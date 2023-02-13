e69d45ac5aa1b54c10d75a4a3055cb35de9d06f7
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-02-13 16:54:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -42,8 +42,6 @@ class FullStackDeveloper(
     BackendDeveloper,
     FrontendDeveloper
 ):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
