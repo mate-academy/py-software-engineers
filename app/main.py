13156366be1d814c6b0e5bfd51ade879232e0164
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-13 15:33:22 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -10,9 +10,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name=name)
-        self.skills += ["JavaScript",
-                        "HTML",
-                        "CSS"]
+        self.skills += ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -43,9 +41,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name=name)
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
