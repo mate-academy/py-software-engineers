d8b3f06536549662a0200891524629e4864a8de3
Igor Starodub <igorastar@gmail.com>
2023-02-27 11:17:07 +0200

    Solo1

--- app/main.py
+++ app/main.py
@@ -43,7 +43,6 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        # self.skills = ["Java", "Android studio"]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
