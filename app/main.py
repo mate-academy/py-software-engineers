b0071d155a96cd533a56ff863f03a77a745c08ae
Bohdan Marko <goldenbohdanm@gmail.com>
2023-03-25 21:47:28 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ class SoftwareEngineer:
         self.name = name
         self.skills = []
 
-    def learn_skill(self, skill : str) -> None:
+    def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
