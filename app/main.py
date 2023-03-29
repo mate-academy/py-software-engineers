cf4648625d436e68781789375635f8f969a724f0
Dmytry Bondariev <bondarievmd@gmail.com>
2023-03-29 14:53:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,10 +9,8 @@ class SoftwareEngineer:
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills += ["JavaScript"]
-        self.skills += ["HTML"]
-        self.skills += ["CSS"]
+        super().__init__(name=name)
+        self.skills += ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -22,9 +20,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills += ["Python"]
-        self.skills += ["SQL"]
-        self.skills += ["Django"]
+        self.skills += ["Python", "SQL", "Django"]
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -34,8 +30,7 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills += ["Java"]
-        self.skills += ["Android studio"]
+        self.skills += ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
