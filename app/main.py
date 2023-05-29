b7537b8294168ca54ae6815905cc06487e8641dd
Viktoriia Krasnovyd <114735598+vkrasnovyd@users.noreply.github.com>
2023-05-29 14:49:13 +0200

    Broken commit

--- app/main.py
+++ app/main.py
@@ -1,10 +1,5 @@
 class SoftwareEngineer:
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.skills = []
-
-    def learn_skill(self, skill: str) -> None:
-        self.skills.append(skill)
+    pass
 
 
 class FrontendDeveloper(SoftwareEngineer):
