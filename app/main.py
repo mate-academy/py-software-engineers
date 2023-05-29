7d9e41b5866625263975c1589f011938fd80ad60
Viktoriia Krasnovyd <114735598+vkrasnovyd@users.noreply.github.com>
2023-05-29 14:50:08 +0200

    Normal commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class SoftwareEngineer:
-    pass
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.skills = []
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
