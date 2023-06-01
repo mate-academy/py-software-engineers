ba79aad8f6963c224014b6d3283197a64411047c
Vadym <seredavadimsv@gmail.com>
2023-06-01 12:17:20 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS"]
+        self.skills += ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
