86cbcc970c3d0f895ce3f068a46e4621871e5d7d
struhanets <posseydon87@gmail.com>
2023-06-07 11:53:04 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -40,8 +40,6 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
 
     def create_web_application(self) -> None:
-        skills = ["Python", "SQL", "Django"]
-        skills += ["JavaScript", "HTML", "CSS"]
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
