865813dd3216f1679a093784234f7bd4e260867e
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-11 15:35:20 +0300

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -51,4 +51,3 @@ class FullStackDeveloper(BackendDeveloper,
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
