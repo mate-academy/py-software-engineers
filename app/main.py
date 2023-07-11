cf3aea49c19b0e1f9f0f91b22861cc48be19a045
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-11 23:07:38 +0300

    fixed init for devs

--- app/main.py
+++ app/main.py
@@ -10,7 +10,6 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.name = name
         self.skills += ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
@@ -21,7 +20,6 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.name = name
         self.skills += ["Python", "SQL", "Django"]
 
     def create_powerful_api(self) -> str:
@@ -32,7 +30,6 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.name = name
         self.skills += ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self) -> str:
