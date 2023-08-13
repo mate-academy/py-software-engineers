068b3958b20f10e615af87e6398cf8d8f7802070
Max Katkalov <makskatkalov@gmail.com>
2023-08-13 22:09:23 +0300

    changed to if

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ class SoftwareEngineer:
         self.skills = []
 
     def learn_skill(self, skill: str) -> None:
-        self.skills.append(skill) if skill not in self.skills else None
+        if skill not in self.skills:
+            self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
