d0d7fdaec887755d8fdffef852ccdeb82a8c084f
Inna Isareva <inna8110302@gmail.com>
2023-08-15 19:39:37 +0300

    fixed mistakes????

--- app/main.py
+++ app/main.py
@@ -39,11 +39,11 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
+
+
+eng = FullStackDeveloper("Ivan")
+print(eng.skills)
