b97cdf9bdd5c4711191629313efaa7686de10279
Olgierrd <oolgierrd@gmail.com>
2023-08-17 16:36:10 +0200

    Solution after changes 1

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS"]
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -21,7 +21,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python", "SQL", "Django"]
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -41,13 +41,6 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python",
-                       "SQL",
-                       "Django",
-                       "JavaScript",
-                       "HTML",
-                       "CSS"
-                       ]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
