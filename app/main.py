0eb2b38003119754ff20729171e65d869ada585c
Poplinskiy Dmytro <super.shepard2@gmail.com>
2023-09-06 18:47:09 +0300

    With problem

--- app/main.py
+++ app/main.py
@@ -1,1 +1,52 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str, skills: list | None = None) -> None:
+        self.name = name
+        if skills is None:
+            skills = []
+        self.skills = skills
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills: list | None = None) -> None:
+        if skills is None:
+            skills = ["JavaScript", "HTML", "CSS"]
+        super().__init__(name=name, skills=skills)
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills: list | None = None) -> None:
+        if skills is None:
+            skills = ["Python", "SQL", "Django"]
+        super().__init__(name=name, skills=skills)
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        skills = ["Java", "Android studio"]
+        super().__init__(name=name, skills=skills)
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper, SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        skills = ["JavaScript", "HTML", "CSS", "Python", "SQL", "Django"]
+        super().__init__(name=name, skills=skills)
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
