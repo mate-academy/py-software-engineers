b7b7d804b0d158cda06aa31bab465b3b10a83b52
Valeriia Danyliuk <bladeryz@gmail.com>
2023-10-10 20:04:05 +0300

    Provided fixes.

--- app/main.py
+++ app/main.py
@@ -43,12 +43,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(
     BackendDeveloper,
-    FrontendDeveloper,
-    SoftwareEngineer
+    FrontendDeveloper
 ):
-
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
