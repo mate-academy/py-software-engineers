fe87d63459d76f53f71d464a9082bda7774fab0a
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-11 13:09:09 +0300

    some moments

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 class SoftwareEngineer:
-
     def __init__(self, name: str) -> None:
         self.name = name
         self.skills = []
@@ -9,7 +8,6 @@ class SoftwareEngineer:
 
 
 class FrontendDeveloper(SoftwareEngineer):
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills.extend(["JavaScript", "HTML", "CSS"])
@@ -20,7 +18,6 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills.extend(["Python", "SQL", "Django"])
@@ -31,7 +28,6 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills.extend(["Java", "Android studio"])
@@ -42,7 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
