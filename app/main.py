7d131764854facd50e0389da885f9af2edb72de7
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-10-11 15:35:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,8 +10,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name=name)
-        for skill in ["JavaScript", "HTML", "CSS"]:
-            self.learn_skill(skill)
+        self.learn_skill.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -21,8 +20,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name=name)
-        for skill in ["Python", "SQL", "Django"]:
-            self.learn_skill(skill)
+        self.learn_skill.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -32,8 +30,7 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name=name)
-        for skill in ["Java", "Android studio"]:
-            self.learn_skill(skill)
+        self.learn_skill.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -41,8 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name=name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
