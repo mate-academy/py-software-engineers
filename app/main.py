0e9c841f8438fb848a6076ff401a3ef5143e9669
Yuliia Dudych <juliadudych@gmail.com>
2023-10-22 23:57:21 +0300

    Define AndroidDeveloper subclass of SoftwareEngineer class

--- app/main.py
+++ app/main.py
@@ -27,3 +27,14 @@ class BackendDeveloper(SoftwareEngineer):
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        for skill in ["Java", "Android studio"]:
+            self.learn_skill(skill)
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
