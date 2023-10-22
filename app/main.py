6cc74712f9b39f58c4d6490ef42ab7ea5da8fced
Yuliia Dudych <juliadudych@gmail.com>
2023-10-22 23:56:59 +0300

    Define BackendDeveloper subclass of SoftwareEngineer class

--- app/main.py
+++ app/main.py
@@ -16,3 +16,14 @@ class FrontendDeveloper(SoftwareEngineer):
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        for skill in ["Python", "SQL", "Django"]:
+            self.learn_skill(skill)
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
