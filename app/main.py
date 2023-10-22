20d7a4fdb670b718a29c64e875740d7a68ab8b2a
Yuliia Dudych <juliadudych@gmail.com>
2023-10-22 23:59:57 +0300

    Define FullStackDeveloper based on Backend- and FrontendDeveloper classes

--- app/main.py
+++ app/main.py
@@ -38,3 +38,13 @@ class AndroidDeveloper(SoftwareEngineer):
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
+
+
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
