51e11ed627196145da8c5ff3fbf251d6321bd803
Ryshtun Denys <rishtund@gmail.com>
2023-11-08 12:03:29 +0200

    ADone (Annotatio problem)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,55 @@
-# write your code here
+import typing
+
+
+class SoftwareEngineer:
+    def __init__(self, name: str, skills=None) -> None:
+        self.name = name
+        self.skills = []
+        if skills:
+            self.skills = skills
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    clas_reqs = ["JavaScript", "HTML", "CSS"]
+
+    def __init__(self, name: str, skills=None) -> None:
+        super().__init__(name, skills)
+        self.skills.extend(FrontendDeveloper.clas_reqs)
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    clas_reqs = ["Python", "SQL", "Django"]
+
+    def __init__(self, name: str, skills=None) -> None:
+        super().__init__(name, skills)
+        self.skills.extend(BackendDeveloper.clas_reqs)
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    clas_reqs = ["Java", "Android studio"]
+
+    def __init__(self, name: str, skills=None) -> None:
+        super().__init__(name, skills)
+        self.skills.extend(AndroidDeveloper.clas_reqs)
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
