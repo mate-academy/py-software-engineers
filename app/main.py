5bd4728678b4aeeb70e80ac1cbbae556d6996379
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2023-11-15 21:17:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name: str) -> None:
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills = ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -20,10 +20,10 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills = ["Python", "SQL", "Django"]
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -31,22 +31,28 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills = ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(AndroidDeveloper,
-                         BackendDeveloper,
+class FullStackDeveloper(BackendDeveloper,
                          FrontendDeveloper,
-                         SoftwareEngineer,
                          ):
-    skills = (FrontendDeveloper.skills + BackendDeveloper.skills)
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills = ["Python",
+                       "SQL",
+                       "Django",
+                       "JavaScript",
+                       "HTML",
+                       "CSS",
+                       ]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
