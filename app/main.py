ae4f99f9f16746ee2374c9ae5c211b90f5ff3727
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-11-16 23:14:15 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,51 @@
-# write your code here
+from typing import List
+
+
+class SoftwareEngineer:
+    def __init__(self, name: str):
+        self.name: str = name
+        self.skills: List[str] = []
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills: List[str] = ["JavaScript", "HTML", "CSS"]
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills: List[str] = ["Python", "SQL", "Django"]
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills: List[str] = ["Java", "Android studio"]
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    def create_web_application(self) -> str:
+        print(f"{self.name} started creating a web application...")
+        api_address: str = self.create_powerful_api()
+        web_page_code: str = self.create_awesome_web_page()
+        return f"Web Application created with API at {api_address} and Web Page:\n{web_page_code}"
