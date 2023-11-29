40153951293b8488b6bc08e340c2b7378114eda8
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-11-29 19:09:04 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,48 +1,48 @@
 class SoftwareEngineer:
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
         self.skills = []
 
-    def learn_skill(self, skill: str):
+    def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
         self.skills.sort()
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills = sorted(["JavaScript", "HTML", "CSS"])
 
-    def create_awesome_web_page(self):
+    def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
 
 class BackendDeveloper(SoftwareEngineer):
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills = sorted(["Python", "SQL", "Django"])
 
-    def create_powerful_api(self):
+    def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills = sorted(["Java", "Android studio"])
 
-    def create_smooth_mobile_app(self):
+    def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
 
-    def create_web_application(self):
+    def create_web_application(self) -> str:
         print(f"{self.name} started creating a web application...")
         api_address = self.create_powerful_api()
         web_page_code = self.create_awesome_web_page()
