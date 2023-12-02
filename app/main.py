f8cfcf6449fbbb890ffa8dd99011bbb196fdf9d4
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2023-12-02 17:21:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -53,4 +53,3 @@ class FullStackDeveloper(BackendDeveloper,
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
