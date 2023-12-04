5e40c14c14d21515b217f46f564f8c5f2252e2f3
Eliza Petrova <elizapetrova8@gmail.com>
2023-12-04 18:33:24 +0200

    py-software-engineers fix

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ class SoftwareEngineer:
 
 
 class FrontendDeveloper(SoftwareEngineer):
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills.extend(["JavaScript", "HTML", "CSS"])
@@ -19,7 +18,6 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills.extend(["Python", "SQL", "Django"])
@@ -30,7 +28,6 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills.extend(["Java", "Android studio"])
@@ -40,12 +37,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(FrontendDeveloper,
-                         BackendDeveloper):
-
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
