6bd24df7b240e02b8fb4fbb9540cb401fbf4faa9
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2023-12-06 13:37:14 +0100

    got rid of redundant inheritance in FullStack class,  py-software-engineers

--- app/main.py
+++ app/main.py
@@ -39,8 +39,7 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(
     BackendDeveloper,
-    FrontendDeveloper,
-    SoftwareEngineer
+    FrontendDeveloper
 ):
 
     def create_web_application(self) -> None:
