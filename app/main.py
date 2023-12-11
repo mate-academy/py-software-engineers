f164270c78e9397e2f5d5e4069a9b62ba04f1e32
Julia Okhrymenko <Ohrimenko15@gmail.com>
2023-12-11 21:43:53 +0200

    solution2

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class SoftwareEngineer:
         self.skills: List[str] = []
 
     def learn_skill(self, skill: str) -> None:
-        self.skills. append(skill)
+        self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
@@ -41,8 +41,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
