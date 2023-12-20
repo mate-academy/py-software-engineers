6bc1945e1d6fc4bbc3ec89f051ef0c77c8632043
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-20 17:16:20 +0200

    improved Solution

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ class FrontendDeveloper(SoftwareEngineer):
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = [
+        self.skills.extend([
             "JavaScript",
             "HTML",
             "CSS",
-        ]
+        ])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -26,11 +26,12 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = [
+
+        self.skills.extend([
             "Python",
             "SQL",
             "Django",
-        ]
+        ])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -40,10 +41,11 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = [
+
+        self.skills.extend([
             "Java",
             "Android studio",
-        ]
+        ])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -51,17 +53,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = [
-            "Python",
-            "SQL",
-            "Django",
-            "JavaScript",
-            "HTML",
-            "CSS",
-        ]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
