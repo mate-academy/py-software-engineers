3bb4feaef6fba4d688e6c9be66942e8e2392506d
Yevhen Tkachuk <Yevhen23tk@gmail.com>
2024-01-25 13:23:13 +0200

    Solution correct ver. 1.1

--- app/main.py
+++ app/main.py
@@ -42,5 +42,3 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-        # BackendDeveloper.create_powerful_api()
-        # FrontendDeveloper.create_awesome_web_page()
