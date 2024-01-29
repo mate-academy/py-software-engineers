671edb06e527ffe2dcb88b2a2d81776c3b850d84
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-01-29 11:25:08 +0200

    unnecessary constructor function was deleted

--- app/main.py
+++ app/main.py
@@ -10,7 +10,6 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-
         self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
@@ -42,9 +41,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
