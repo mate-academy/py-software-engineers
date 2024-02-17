673d2a14f86a72a8f11ad66a750552ca903b2583
Andrei Butko <andreybutkomerefa@gmail.com>
2024-02-17 20:53:53 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -30,15 +30,14 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Java", "Android studio"]
+        self.skills += ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper,
-                         FrontendDeveloper,):
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
