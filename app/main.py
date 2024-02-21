9c6e6b38befddb23de2832f5ea0d3b9d67898a5d
Mikola Rozhansky <bymikola@gmail.com>
2024-02-21 21:13:21 +0200

    All_test_ok

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
-class SoftwareEngіneer:
+class SoftwareEngineer:
     skills = []
 
     def __init__(self, name: str) -> None:
         self.name = name
 
     @classmethod
-    def learn_skill(cls, skill: str):
+    def learn_skill(cls, skill: str) -> None:
         cls.skills.append(skill)
 
 
-class FrontendDeveloper(SoftwareEngіneer):
+class FrontendDeveloper(SoftwareEngineer):
     skills = ["JavaScript", "HTML", "CSS"]
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
 
     def create_awesome_web_page(self) -> str:
@@ -20,21 +20,21 @@ class FrontendDeveloper(SoftwareEngіneer):
         return "<h1>Hello world</h1>"
 
 
-class BackendDeveloper(SoftwareEngіneer):
+class BackendDeveloper(SoftwareEngineer):
     skills = ["Python", "SQL", "Django"]
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
 
-    def create_powerful_api(self) ->str:
+    def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
-class AndroidDeveloper(SoftwareEngіneer):
+class AndroidDeveloper(SoftwareEngineer):
     skills = ["Java", "Android studio"]
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
 
     def create_smooth_mobile_app(self) -> str:
@@ -45,15 +45,7 @@ class AndroidDeveloper(SoftwareEngіneer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
 
-    def create_web_application(self):
+    def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
-
-
-
-
-
-
-
