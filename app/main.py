b494bc3777c4b09d315dbae7133e867215acd232
Mikola Rozhansky <bymikola@gmail.com>
2024-02-24 12:32:25 +0200

    All_test_ok

--- app/main.py
+++ app/main.py
@@ -48,4 +48,5 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
+
         self.create_awesome_web_page()
