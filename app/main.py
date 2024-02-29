7996463c4185a42d4173d486754c7387d4084ec2
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-02-29 17:33:24 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -10,9 +10,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        skills = ["JavaScript", "HTML", "CSS"]
-        for skill in skills:
-            self.learn_skill(skill)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -22,9 +20,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        skills = ["Python", "SQL", "Django"]
-        for skill in skills:
-            self.learn_skill(skill)
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -34,9 +30,7 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        skills = ["Java", "Android studio"]
-        for skill in skills:
-            self.learn_skill(skill)
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
