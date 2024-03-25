5fe48cfdd2c0df35e2ffdb2faf8f5db4df1be142
Olexandr Syrojeskin <114597871+CharmNiovi@users.noreply.github.com>
2024-03-25 14:06:05 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ class SoftwareEngineer:
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.name = name
+        super().__init__(name)
         self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
@@ -38,9 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
