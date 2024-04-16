bf78de47e078fc10d9799a6973a5b7b7d2b80910
Anna Shevtsova <aniashevtsova@gmail.com>
2024-04-16 17:31:31 +0200

    comments adressed

--- app/main.py
+++ app/main.py
@@ -40,9 +40,6 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        SoftwareEngineer.skills = [
-            "Python", "SQL", "Django", "JavaScript", "HTML", "CSS"
-        ]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
