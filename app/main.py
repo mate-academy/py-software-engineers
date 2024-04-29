5fe74e902ebbb551fddf013d1947d9b1a01c1d28
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-04-29 14:07:00 +0300

    Fix solution

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = [
+        self.skills += [
             "JavaScript",
             "HTML",
             "CSS"
@@ -50,16 +50,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper,
                          FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = [
-            "Python",
-            "SQL",
-            "Django",
-            "JavaScript",
-            "HTML",
-            "CSS"
-        ]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
