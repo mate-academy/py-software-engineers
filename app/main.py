e3df44da2c70b7e397818493edae3c4334cb37f9
Olena Zaritska <zaritskaos@gmail.com>
2024-05-01 13:19:04 +0300

    remove __init__ from class FullStackDeveloper

--- app/main.py
+++ app/main.py
@@ -38,9 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
