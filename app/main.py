8de052584cce7e0d01d0a572e39e2a353dad796d
Mykola <jacktyler2391@gmail.com>
2024-06-20 15:53:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,63 @@
-# write your code here
+class SoftwareEngineer:
+    skills = []
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    skills = ["JavaScript", "HTML", "CSS"]
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def create_awesome_web_page(self) -> str:
+        page = "<h1>Hello world</h1>"
+        print(f"{self.name} is creating a webpage...")
+        return page
+
+
+class BackendDeveloper(SoftwareEngineer):
+    skills = ["Python", "SQL", "Django"]
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def create_powerful_api(self) -> str:
+        address = "http://127.0.0.1:8000"
+        print(f"{self.name} is creating an API...")
+        return address
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    skills = ["Java", "Android studio"]
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def create_smooth_mobile_app(self) -> str:
+        app = "Ads every three swipes"
+        print(f"{self.name} is creating a mobile app...")
+        return app
+
+
+class FullStackDeveloper(AndroidDeveloper,
+                         BackendDeveloper,
+                         FrontendDeveloper,
+                         SoftwareEngineer):
+
+    skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
+
+
+full_stack_dev = FullStackDeveloper("Tom")
+
+print(full_stack_dev.skills)
+print(full_stack_dev.create_web_application())
