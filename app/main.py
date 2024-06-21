0d38aafc45eb62462321cfb55cdaa12155f79814
Mykola <jacktyler2391@gmail.com>
2024-06-21 11:10:57 +0300

    code for test

--- app/main.py
+++ app/main.py
@@ -9,47 +9,39 @@ class SoftwareEngineer:
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
-        page = "<h1>Hello world</h1>"
         print(f"{self.name} is creating a webpage...")
-        return page
+        return "<h1>Hello world</h1>"
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
-        address = "http://127.0.0.1:8000"
         print(f"{self.name} is creating an API...")
-        return address
+        return "http://127.0.0.1:8000"
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
-        app = "Ads every three swipes"
         print(f"{self.name} is creating a mobile app...")
-        return app
+        return "Ads every three swipes"
 
 
-class FullStackDeveloper(AndroidDeveloper,
-                         BackendDeveloper,
-                         FrontendDeveloper,
-                         SoftwareEngineer):
-
-    skills = FrontendDeveloper.skills + BackendDeveloper.skills
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
