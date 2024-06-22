d7b9ab97cb36894f68d46987abc6a386ccda5fc5
Mykola <jacktyler2391@gmail.com>
2024-06-22 08:38:33 +0300

    final fix to code style

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name: str) -> None:
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
