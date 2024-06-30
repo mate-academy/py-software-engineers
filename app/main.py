f2c9b55516dbede3795ddc904a2d4bda81516a59
Alex <54539kalashnikov@gmail.com>
2024-06-30 18:46:56 +0300

    dell redundant method

--- app/main.py
+++ app/main.py
@@ -51,11 +51,11 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills.extend([
+        self.skills += [
             "JavaScript",
             "HTML",
             "CSS"
-        ])
+        ]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
