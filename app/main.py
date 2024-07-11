8f6ea3e15991212e7a58059c5712c2fc6162b710
Daria Ganchukova <umi194@gmail.com>
2024-07-11 10:05:35 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS", "Python", "SQL", "Django"]
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
