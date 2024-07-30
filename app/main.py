f91ddd61495d8c9ba94f78a2ac6c7385ed25d7e5
Alexandr Politov <apolitov83@gmail.com>
2024-07-30 10:59:35 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -38,10 +38,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-        BackendDeveloper.create_powerful_api(self)
-        FrontendDeveloper.create_awesome_web_page(self)
+        self.create_powerful_api()
+        self.create_awesome_web_page()
