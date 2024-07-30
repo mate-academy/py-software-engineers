2b6417187eb9340cfe8ae1998f7d43ffcf1f173b
Maxim Turenko <turenkomaksim099@gmail.com>
2024-07-30 08:47:03 +0300

    implement py-software-engineers

--- app/main.py
+++ app/main.py
@@ -45,3 +45,4 @@ class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
+
