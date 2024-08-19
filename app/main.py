fb9785e6399e32c44cac6799ddb928a5195ce0ea
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-19 13:11:43 +0300

    add SoftwareEngineer class

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.skills = []
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
