2de5304a4145579b80d3b99206ac48259aec46cc
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-19 19:21:31 +0300

    code refactor

--- app/main.py
+++ app/main.py
@@ -38,12 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = []
-        self.skills.extend(BackendDeveloper(name).skills)
-        self.skills.extend(FrontendDeveloper(name).skills)
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
