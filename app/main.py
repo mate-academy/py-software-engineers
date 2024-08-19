a05659ad3c8f0e160fd5237e51bb6ead84d46fa2
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-19 13:14:06 +0300

    add FrontendDeveloper class

--- app/main.py
+++ app/main.py
@@ -5,3 +5,13 @@ class SoftwareEngineer:
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills = ["JavaScript", "HTML", "CSS"]
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
