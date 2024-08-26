aab83cefc49a80058e3132fc608aba2094951c75
Nastya Kalinina <kalininan.kalinina@gmail.com>
2024-08-26 20:11:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,16 @@
 class SoftwareEngineer:
-    SKILLS = []
-
     def __init__(self, name: str) -> None:
         self.name = name
-        self.skills = self.SKILLS
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    SKILLS = ["JavaScript", "HTML", "CSS"]
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = self.SKILLS
+        self.skills += ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -22,11 +18,9 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    SKILLS = ["Python", "SQL", "Django"]
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = self.SKILLS
+        self.skills += ["Python", "SQL", "Django"]
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -34,11 +28,9 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    SKILLS = ["Java", "Android studio"]
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = self.SKILLS
+        self.skills += ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -46,10 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = BackendDeveloper.SKILLS + FrontendDeveloper.SKILLS
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
