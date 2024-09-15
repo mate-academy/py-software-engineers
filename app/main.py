a3866456936fd0b1a447de09c680b4468273ea91
Lev <lev2099@gmail.com>
2024-09-15 17:42:11 +0300

    delete init in FullStack plus skills in class use self.create_powerful_api and self.create_awesome_web_page

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS", ]
+        self.skills += ["JavaScript", "HTML", "CSS", ]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -20,7 +20,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python", "SQL", "Django", ]
+        self.skills += ["Python", "SQL", "Django", ]
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -30,7 +30,7 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Java", "Android studio", ]
+        self.skills += ["Java", "Android studio", ]
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -38,15 +38,10 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper,
-                         BackendDeveloper,
-                         AndroidDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = ["Python", "SQL", "Django",
-                       "JavaScript", "HTML", "CSS", ]
+                         BackendDeveloper):
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-        print(f"{self.name} is creating an API...")
-        print(f"{self.name} is creating a webpage...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
         return
