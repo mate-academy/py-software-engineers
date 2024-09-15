140c5640047b1270dfaaade48f189f0b9b4cebac
Lev <lev2099@gmail.com>
2024-09-15 17:23:02 +0300

    delete Mixins

--- app/main.py
+++ app/main.py
@@ -7,40 +7,34 @@ class SoftwareEngineer:
         self.skills.append(skill)
 
 
-class FrontDevMixin:
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills = ["JavaScript", "HTML", "CSS", ]
+
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
 
-class BackDevMixin:
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills = ["Python", "SQL", "Django", ]
+
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
-class AndroidDevMixin:
-    def create_smooth_mobile_app(self) -> str:
-        print(f"{self.name} is creating a mobile app...")
-        return "Ads every three swipes"
-
-
-class FrontendDeveloper(SoftwareEngineer, FrontDevMixin):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS", ]
-
-
-class BackendDeveloper(SoftwareEngineer, BackDevMixin):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = ["Python", "SQL", "Django", ]
-
-
-class AndroidDeveloper(SoftwareEngineer, AndroidDevMixin):
+class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.skills = ["Java", "Android studio", ]
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
 
 
 class FullStackDeveloper(FrontendDeveloper,
