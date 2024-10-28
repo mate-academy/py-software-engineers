dd20bf812f6658dd2b6942cd7205708755783e6c
Vova Savenkov <sava.v80@gmail.com>
2024-10-28 13:35:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -42,3 +42,4 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
+# end
