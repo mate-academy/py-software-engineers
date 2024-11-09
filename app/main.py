a63c702d77ae25e7509a26beb7638fec983183fc
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-11-09 12:58:23 +0100

    fixed 5

--- app/main.py
+++ app/main.py
@@ -45,8 +45,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
-        FrontendDeveloper.__init__(self, name)
-        BackendDeveloper.__init__(self, name)
+        super().__init__(name)
+        self.skills = list(set(self.skills))
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
