d5cc09ef32524d80f906a27deb9915ef75ad2e66
Oleh <sholeg2005@gmail.com>
2024-11-16 02:26:16 +0100

    'Sol'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 class SoftwareEngineer:
+    skills = []
+
     def __init__(self, name: str) -> None:
         self.name = name
         self.skills = []
 
     def learn_skill(self, skill: str) -> None:
-        self.skills.append(skill)
+        if skill not in self.skills:
+            self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
+    skills = ["JavaScript", "HTML", "CSS"]
+
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS"]
+        self.skills = FrontendDeveloper.skills.copy()
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -18,9 +23,11 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
+    skills = ["Python", "SQL", "Django"]
+
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python", "SQL", "Django"]
+        self.skills = BackendDeveloper.skills.copy()
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -28,9 +35,11 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
+    skills = ["Java", "Android studio"]
+
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Java", "Android studio"]
+        self.skills = AndroidDeveloper.skills.copy()
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -39,10 +48,14 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = list(set(FrontendDeveloper(name).skills + BackendDeveloper(name).skills))
+        FrontendDeveloper.__init__(self, name)
+        BackendDeveloper.__init__(self, name)
+        # Combine skills from both parent classes, ensuring no duplicates
+        self.skills = list(set(FrontendDeveloper.skills +
+                               BackendDeveloper.skills))
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
+
