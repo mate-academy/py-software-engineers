67a8d83416cffa9f22b414bc704db958d36ca851
Bohdan Tereschenkov <jonieblack1488@gmail.com>
2024-12-06 23:20:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -38,7 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-
     def __init__(self, name: str) -> None:
         super().__init__(name)
 
