bbca42f3ef865189f6639886f490e49555d1c4fa
D Petetskyi <4477617@gmail.com>
2024-12-17 10:35:56 +0200

    replaced + to extend

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def create_web_application(self) -> str:
+    def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
