3e3e92097caf3b4863444484976de36ff6601cd6
Vlad <vladlubeka147@gmail.com>
2024-12-18 19:10:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str):
+        self.name = name
+        self.skills = []
+
+    def learn_skill(self, skill: str):
+        if skill not in self.skills:
+            self.skills.append(skill)
+
+    def __repr__(self):
+        return f"{self.name} has skills: {', '.join(self.skills)}"
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills.extend(["JavaScript", "CSS", "HTML"])
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills.extend(["Python", "SQL", "Django"])
+
+
+class FullStackDeveloper(SoftwareEngineer):
+    def __init__(self, name: str):
+        super().__init__(name)
+        # Объединяем навыки Frontend и Backend
+        self.skills.extend(["JavaScript", "CSS", "HTML", "Python", "SQL", "Django"])
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills.extend(["Java", "Android Studio"])
