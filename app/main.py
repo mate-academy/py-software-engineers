60cad8a37e47b1e9de75bcd93221836b42f63733
Volodymir Shlikhta <shlikhta77@gmail.com>
2025-01-15 22:28:14 +0200

    Implement type hints for SoftwareEngineer and subclasses

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from typing import List
 
 class SoftwareEngineer:
     def __init__(self, name: str) -> None:
-        self.name = name
+        self.name: str = name
         self.skills: List[str] = []
 
     def learn_skill(self, skill: str) -> None:
@@ -13,7 +13,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["JavaScript", "HTML", "CSS"]
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -23,7 +23,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python", "SQL", "Django"]
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -33,7 +33,7 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Java", "Android studio"]
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -43,7 +43,8 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
+        # Remove duplicate skills if necessary
+        self.skills = list(dict.fromkeys(self.skills))
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
