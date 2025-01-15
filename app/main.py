010a64f9e2a4e0c774c0fdff1970e3a8a42155c3
Mykyta <mitina1900@gmail.com>
2025-01-15 22:06:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -41,10 +41,8 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         BackendDeveloper.__init__(self, name)
         FrontendDeveloper.__init__(self, name)
-        self.skills = (
-                BackendDeveloper(name).skills +
-                FrontendDeveloper(name).skills
-        )
+        self.skills = BackendDeveloper(name).skills + \
+                      FrontendDeveloper(name).skills)
 
     def create_web_application(self) -> str:
         print(f"{self.name} started creating a web application...")
