611e95aa536b4812c4311dd5bb168da2668e428a
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-01-23 14:23:32 +0100

    change in FullStackDeveloper _init_'s method

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
+        super().__init__(name=name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
