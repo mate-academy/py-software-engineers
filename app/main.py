aa37ffbbc653135f9f15ceb2327053ceb470bd43
RuslanTopal <baranotik15@gmail.com>
2025-02-04 20:39:55 +0000

    add extend and correct my mistake

--- app/main.py
+++ app/main.py
@@ -10,9 +10,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.learn_skill("JavaScript")
-        self.learn_skill("HTML")
-        self.learn_skill("CSS")
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
