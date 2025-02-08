51e4faff00fe61a8105e53d914e01b935d532221
Roman Kostiv <rkostiv@gmail.com>
2025-02-09 01:07:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -25,7 +25,6 @@ class BackendDeveloper(SoftwareEngineer):
         super().__init__(name)
         self.skills.extend(["Python", "SQL", "Django"])
 
-
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
