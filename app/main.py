1ff05e74c896dac8f303ad4356e938128dd6e06a
oleanto <olegantonenko100@gmail.com>
2025-03-04 17:31:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class FrontendDeveloper(SoftwareEngineer):
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
-        return f"<h1>Hello world</h1>"
+        return "<h1>Hello world</h1>"
 
 
 class BackendDeveloper(SoftwareEngineer):
@@ -25,7 +25,7 @@ class BackendDeveloper(SoftwareEngineer):
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
-        return f"http://127.0.0.1:8000"
+        return "http://127.0.0.1:8000"
 
 
 class AndroidDeveloper(SoftwareEngineer):
@@ -35,7 +35,7 @@ class AndroidDeveloper(SoftwareEngineer):
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
-        return f"Ads every three swipes"
+        return "Ads every three swipes"
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
