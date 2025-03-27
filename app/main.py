16a89dd6b61866559335cfa36b0386a0a8d3a628
Denys <kolbasin1990@gmail.com>
2025-03-28 00:15:18 +0200

    py_software_engineers_v2

--- app/main.py
+++ app/main.py
@@ -46,3 +46,4 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
+
