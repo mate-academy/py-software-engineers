7997a3a6259105c030336fc19634d13ecc9a7020
Oleksandr Kozlov <aleksandrkozlov2015@gmail.com>
2025-03-29 13:44:45 +0100

    'Solution_1'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,69 @@
-# write your code here
+from __future__ import annotations
+
+
+class SoftwareEngineer:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.skills = []
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skill = ["JavaScript", "HTML", "CSS"]
+        self.skills.extend(self.skill)
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skill = ["Python", "SQL", "Django"]
+        self.skills.extend(self.skill)
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skill = ["Java", "Android studio"]
+        self.skills.extend(self.skill)
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
+
+
+full_stack_dev = FullStackDeveloper("Tom")
+# full_stack_dev.skills == [
+#     "Python",
+#     "SQL",
+#     "Django",
+#     "JavaScript",
+#     "HTML",
+#     "CSS",
+# ]
+
+full_stack_dev.create_web_application()
+# Tom started creating a web application...
+# Tom is creating an API...
+# Tom is creating a webpage...
