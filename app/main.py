5ef0ce33270d158e7d21d5b1ac60cec297fe0da2
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-04-10 20:38:38 +0200

    change init in  fullstack class

--- app/main.py
+++ app/main.py
@@ -52,7 +52,9 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper,
                          BackendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name=name)
+        SoftwareEngineer.__init__(self, name=name)
+        self.f_skills_extend(self.skills)
+        self.b_skills_extend(self.skills)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
