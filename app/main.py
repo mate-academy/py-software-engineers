a978840f306bf7779953d8c687bf9d8fb7ba8789
Oleksii Drenov <leshadrenev@gmail.com>
2025-04-13 23:52:03 +0300

    fixed solution

--- app/main.py
+++ app/main.py
@@ -48,9 +48,5 @@ class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-        super().create_powerful_api()
-        super().create_awesome_web_page()
-
-
-full_stack_dev = FullStackDeveloper("Tom")
-print(full_stack_dev.skills)
+        BackendDeveloper.create_powerful_api(self)
+        FrontendDeveloper.create_awesome_web_page(self)
