ee062fc72ca33db6f185c2704f58fd9471ec5dc1
Roman Azhniuk <roman.azhniuk@gmail.com>
2025-04-26 20:55:49 +0300

    implement py-software-engineers

--- app/main.py
+++ app/main.py
@@ -44,8 +44,8 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         SoftwareEngineer.__init__(self, name)
-        # Get skills from both parent classes and combine them into a set to remove duplicates
-        self.skills = list(set(BackendDeveloper(name).skills + FrontendDeveloper(name).skills))
+        self.skills = list(set(BackendDeveloper(name).skills +
+                               FrontendDeveloper(name).skills))
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
