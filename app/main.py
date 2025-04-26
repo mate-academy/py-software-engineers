52298d859cdd1f9140e1063512d14c0f5ffb4c23
Danylo-D87 <quaestor88@gmail.com>
2025-04-26 17:00:19 +0300

    sol 3

--- app/main.py
+++ app/main.py
@@ -49,7 +49,8 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
     def __init__(self, name: str) -> None:
-        super().__init__(name)
+        BackendDeveloper.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
 
         self.skills.extend([
             "Python", "SQL", "Django",  # Backend
