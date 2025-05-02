d3b7907db6fc52c6362e35ff2e5c5f5a3b517af5
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-05-02 16:00:43 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -44,8 +44,11 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
     def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills = list(set(self.skills))
+        SoftwareEngineer.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
+        BackendDeveloper.__init__(self, name)
+        self.skills = list(dict.fromkeys(self.skills))
+
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
