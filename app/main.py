685d9e6c316b370f46100bac1765d6c9449ff875
Dimon <kosma4evskuj@gmail.com>
2025-05-04 11:48:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -40,11 +40,7 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        unique_skills: list[str] = []
-        for skill in self.skills:
-            if skill not in unique_skills:
-                unique_skills.append(skill)
-        self.skills = unique_skills
+        self.skills = list(dict.fromkeys(self.skills))
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
