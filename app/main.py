97b3a8bcd365c6f90a818f69b263cd9821d404b7
m3 <sergey.radik@gmail.com>
2025-05-10 19:14:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -39,6 +39,11 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(self, name: str) -> None:
+        SoftwareEngineer.__init__(self, name)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])   # Frontend skills
+        self.skills.extend(["Python", "SQL", "Django"])     # Backend skills
+
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
