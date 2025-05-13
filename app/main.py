1871757cf401dbb6b68427d9e3c3c8b78123e9f7
Oleksandr Dziuba <tornadodzu122@gmail.com>
2025-05-14 00:44:59 +0300

    Solution_13/05/25_Dziuba_V3

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 class SoftwareEngineer:
     def __init__(self, name: str) -> None:
         self.name = name
-
-    skills = []
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    skills = ["JavaScript", "HTML", "CSS"] + SoftwareEngineer.skills
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills += ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -17,7 +18,9 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
-    skills = ["Python", "SQL", "Django"] + SoftwareEngineer.skills
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills += ["Python", "SQL", "Django"]
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -25,7 +28,9 @@ class BackendDeveloper(SoftwareEngineer):
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    skills = ["Java", "Android studio"] + SoftwareEngineer.skills
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.skills += ["Java", "Android studio"]
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
@@ -34,8 +39,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
 
-    skills = (BackendDeveloper.skills + FrontendDeveloper.skills
-              + SoftwareEngineer.skills)
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
