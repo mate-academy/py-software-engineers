f1f99d6a9b0ba9b4b6bf40dfaf1f1d9b107e7d2b
Gheifr <48731758+Gheifr@users.noreply.github.com>
2025-05-13 16:01:37 +0300

    Solution 3

--- app/main.py
+++ app/main.py
@@ -46,9 +46,9 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.learn_skill("HTML")
-        self.learn_skill("CSS")
-        self.learn_skill("JavaScript")
+        self.learn_skill("Python")
+        self.learn_skill("Django")
+        self.learn_skill("SQL")
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
