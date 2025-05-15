3ba9892fe60948bb08ee4449a41fd0abac3d187a
Andrii Omelchenko <andreyomem@gmail.com>
2025-05-15 18:01:57 +0300

    change __init__ method

--- app/main.py
+++ app/main.py
@@ -38,12 +38,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
-        self.skills = ["Python", "SQL", "Django","JavaScript", "HTML", "CSS"]
-
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
