7eeb2687e30a1021ad9b8b7f0cf6c065dab3a149
nkkmkk <nikitmkk@gmail.com>
2025-05-16 11:03:10 +0200

    fix __init__ to FullStackDeveloper

--- app/main.py
+++ app/main.py
@@ -39,7 +39,9 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
+        SoftwareEngineer.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
+        BackendDeveloper.__init__(self, name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
