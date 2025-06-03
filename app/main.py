3a444a4bd8ed0391192df4c0c0cc4783d69c3e1c
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-06-02 21:23:07 -0300

    resolution

--- app/main.py
+++ app/main.py
@@ -50,8 +50,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(
-    BackendDeveloper,
-    FrontendDeveloper
+    FrontendDeveloper,
+    BackendDeveloper
 ):
 
     def __init__(self, name: str) -> None:
