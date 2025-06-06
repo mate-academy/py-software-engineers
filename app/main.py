3e1e1e101e858989c09cbfd4f2557cddaee0b376
Gabriel Fray <gfraycastilho@gmail.com>
2025-06-06 00:37:03 -0300

    Solve

--- app/main.py
+++ app/main.py
@@ -46,3 +46,4 @@ class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
+fd
