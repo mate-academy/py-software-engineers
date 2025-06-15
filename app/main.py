e462865e531019ca4b3b7d05a1d9dad523925591
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-06-15 23:35:53 +0200

    fixed-mistake-in-FrontendDeveloper

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class FrontendDeveloper(SoftwareEngineer):
         self.skills = ["JavaScript", "HTML", "CSS"]
 
     def create_awesome_web_page(self) -> str:
-        print(f"{self.name} creating a webpage...")
+        print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
 
