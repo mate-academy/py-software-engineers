36f3655b36f0b98816e63320ed6436c12c969fd4
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-06-18 01:55:59 +0700

    my code IS correct, but AI demands to ensure, that second parent class would being invoked. Thus, I am trying some stuff

--- app/main.py
+++ app/main.py
@@ -38,8 +38,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
+    def __init__(self, name: str, **kwargs) -> None:
+        super().__init__(name, **kwargs)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
