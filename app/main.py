8302c7d6fcc64d0258292b39dfa16bc8caf28b3c
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-06-18 13:20:21 +0700

    Due to AI demand I changed Full, Back and Front classes:

--- app/main.py
+++ app/main.py
@@ -8,9 +8,11 @@ class SoftwareEngineer:
 
 
 class FrontendDeveloper(SoftwareEngineer):
+    s_skills = ["JavaScript", "HTML", "CSS"]
+
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills.extend(["JavaScript", "HTML", "CSS"])
+        self.skills.extend(FrontendDeveloper.s_skills)
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -18,9 +20,11 @@ class FrontendDeveloper(SoftwareEngineer):
 
 
 class BackendDeveloper(SoftwareEngineer):
+    s_skills = ["Python", "SQL", "Django"]
+
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills.extend(["Python", "SQL", "Django"])
+        self.skills.extend(BackendDeveloper.s_skills)
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -38,8 +42,11 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str, **kwargs) -> None:
-        super().__init__(name, **kwargs)
+    def __init__(self, name: str) -> None:
+        # That's how it was: super().__init__(name)
+        # but, due to AI demands, I had to change all those 3 classes
+        super().__init__(name)
+        self.skills = BackendDeveloper.s_skills + FrontendDeveloper.s_skills
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
