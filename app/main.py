f1f2bb18f61b78719ecad5ec58a0fefa33675dcb
Pavlo Semenikhin <pashock1337@gmail.com>
2025-06-29 21:50:37 +0300

    changed solution py software engineers

--- app/main.py
+++ app/main.py
@@ -38,8 +38,18 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        SoftwareEngineer.__init__(self, name)
+        self.skills.extend([
+            "Python",
+            "SQL",
+            "Django",
+            "JavaScript",
+            "HTML",
+            "CSS"
+        ])
+
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
-
         self.create_powerful_api()
         self.create_awesome_web_page()
