c5eb46168ab67b70270375cd52cd6b34b62a2b91
Artcrafterrra <if.artcraft@gmail.com>
2025-06-30 17:23:39 +0300

    part

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.skills = []
+
+    def learn_skill(self, skill: str):
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__()
+        self.skills
