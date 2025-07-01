bd3fb90bcdbe2d06065433603d0d3e6a9d978047
SensibleN00B <sensible.noob@gmail.com>
2025-07-01 09:15:30 +0300

    final version of init in fullStackDeveloper v2

--- app/main.py
+++ app/main.py
@@ -52,7 +52,3 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
-
-full = FullStackDeveloper("Andrew")
-print(full.skills)
