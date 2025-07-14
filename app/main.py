34ec7e71a0adde433eee0ed2b8c8e7dc26d0b468
Nick Konkov <kharkivnick@gmail.com>
2025-07-14 15:27:25 +0300

    Implementation via mixins

--- app/main.py
+++ app/main.py
@@ -7,30 +7,51 @@ class SoftwareEngineer:
         self.skills.append(skill)
 
 
-class FrontendDeveloper(SoftwareEngineer):
+class FrontendDeveloperMixin:
+    def add_frontend(self) -> None:
+        dev_skills = ["JavaScript", "HTML", "CSS"]
+        for skill in dev_skills:
+            self.learn_skill(skill)
+
+
+class BackendDeveloperMixin:
+    def add_backend(self) -> None:
+        dev_skills = ["Python", "SQL", "Django"]
+        for skill in dev_skills:
+            self.learn_skill(skill)
+
+
+class AndroidDeveloperMixin:
+    def add_android(self) -> None:
+        dev_skills = ["Java", "Android studio"]
+        for skill in dev_skills:
+            self.learn_skill(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer, FrontendDeveloperMixin):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = self.skills + ["JavaScript", "HTML", "CSS"]
+        self.add_frontend()
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
 
-class BackendDeveloper(SoftwareEngineer):
+class BackendDeveloper(SoftwareEngineer, BackendDeveloperMixin):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = self.skills + ["Python", "SQL", "Django"]
+        self.add_backend()
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
-class AndroidDeveloper(SoftwareEngineer):
+class AndroidDeveloper(SoftwareEngineer, AndroidDeveloperMixin):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = self.skills + ["Java", "Android studio"]
+        self.add_android()
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
