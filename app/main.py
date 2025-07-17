7522dd18ceda6a8ad92db92eb43fe39cf65a3c17
Mateusz <szanieckimateusz@gmail.com>
2025-07-17 14:42:14 +0200

    Completed methods with adding skills to the list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 class SoftwareEngineer:
-    skills = []
 
     def __init__(self, name: str) -> None:
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
@@ -10,7 +10,10 @@ class SoftwareEngineer:
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.name = name
+        super().__init__(name)
+        self.learn_skill("JavaScript")
+        self.learn_skill("HTML")
+        self.learn_skill("CSS")
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -19,7 +22,10 @@ class FrontendDeveloper(SoftwareEngineer):
 
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.name = name
+        super().__init__(name)
+        self.learn_skill("Python")
+        self.learn_skill("SQL")
+        self.learn_skill("Django")
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -28,7 +34,9 @@ class BackendDeveloper(SoftwareEngineer):
 
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.name = name
+        super().__init__(name)
+        self.learn_skill("Java")
+        self.learn_skill("Android studio")
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
