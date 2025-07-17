cab3b1c5aab7c78e583a7ba7c2e11faf878e13c6
Mateusz <szanieckimateusz@gmail.com>
2025-07-17 14:19:45 +0200

    fixing flake8 requirements

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 class SoftwareEngineer:
     skills = []
+
     def __init__(self, name: str) -> None:
         self.name = name
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
+
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
@@ -14,6 +16,7 @@ class FrontendDeveloper(SoftwareEngineer):
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello World!</h1>"
 
+
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         self.name = name
@@ -21,6 +24,7 @@ class BackendDeveloper(SoftwareEngineer):
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
+
 
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
@@ -30,8 +34,9 @@ class AndroidDeveloper(SoftwareEngineer):
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
+
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def create_web_application(self):
+    def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
