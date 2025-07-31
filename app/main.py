1713d8b4699c0fdd1a54eb555b25a2f5f1454f2c
Konstantin S. <kostaho16@gmail.com>
2025-07-31 23:41:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ class SoftwareEngineer:
         else:
             raise TypeError(f"Type {type(skill)} is not supported")
 
+
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
