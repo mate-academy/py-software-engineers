b61323e2e038638bd5641727ea4d59bb21ad84f3
Yurii Palahitskyi <brebston7@gmail.com>
2025-08-03 10:36:09 +0200

    Fixed issues

--- app/main.py
+++ app/main.py
@@ -41,6 +41,11 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(self, name: str) -> None:
+        SoftwareEngineer.__init__(self, name)
+        self.skills += ["JavaScript", "HTML", "CSS"]
+        self.skills += ["Python", "SQL", "Django"]
+
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
