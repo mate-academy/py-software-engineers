13be3f93ce663d32ddf4559421db7e5979ec01b1
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-08-13 22:48:13 +0300

    implemented py-software-engineers v3

--- app/main.py
+++ app/main.py
@@ -39,7 +39,8 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
+        SoftwareEngineer.__init__(self, name)
+        self.skills += ["Python", "SQL", "Django", "JavaScript", "CSS", "HTML"]
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
