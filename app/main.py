b271f1a6589ae9db10118dde88cd89519355a857
Gena <gena.anzin@ukr.net>
2025-08-26 17:35:54 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+        self.skills = []
+    def learn_skill(self, new_skills) -> None:\False
