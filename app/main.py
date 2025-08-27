a238cf3790963aff8c0f599878ae80f2d7352e0f
AKupriichuk <alexa05kup@gmail.com>
2025-08-27 19:06:11 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ class FrontendDeveloper(SoftwareEngineer):
         self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
-        print(f'{self.name} is creating a webpage...')
+        print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
 
@@ -23,7 +23,7 @@ class BackendDeveloper(SoftwareEngineer):
         self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
-        print(f'{self.name} is creating an API...')
+        print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
@@ -33,7 +33,7 @@ class AndroidDeveloper(SoftwareEngineer):
         self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
-        print(f'{self.name} is creating a mobile app...')
+        print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
