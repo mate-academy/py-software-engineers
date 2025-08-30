6b623fc87c76322417f6b5d68f73cc7fcc9911b6
vasylhladyshev404 <vasyl@nextgear.nl>
2025-08-30 13:24:50 +0200

    1

--- app/main.py
+++ app/main.py
@@ -33,7 +33,6 @@ class AndroidDeveloper(SoftwareEngineer):
         super().__init__(name)
         self.skills += ["Java", "Android studio"]
 
-
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
