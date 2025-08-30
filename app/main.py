2d7a681e102e9df27037093b065c24624e9c014f
vasylhladyshev404 <vasyl@nextgear.nl>
2025-08-30 13:21:06 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,39 +1,40 @@
 class SoftwareEngineer:
     skills = []
+
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def learn_skill(skill: str) -> None:
+    def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.super().__init__(name)
+        super().__init__(name)
         self.skills += ["JavaScript", "HTML", "CSS"]
 
-    def create_awesome_web_page() -> str:
+    def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
 
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.super().__init__(name)
+        super().__init__(name)
         self.skills += ["Python", "SQL", "Django"]
 
-    def create_powerful_api() -> str:
+    def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.super().__init__(name)
+        super().__init__(name)
         self.skills.append("Java")
         self.skills.append("Android studio")
 
-    def create_smooth_mobile_app() -> str:
+    def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
