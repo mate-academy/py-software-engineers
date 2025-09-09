38076470d3d7c51808440bf6a54be3f1a5587a99
0lex1y <kotov.oleksiy@icloud.com>
2025-09-09 03:14:10 +0300

    Fix self.skills

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 class SoftwareEngineer:
-    skills = []
-
     def __init__(self, name: str) -> None:
         self.name = name
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
