0a52fee842d9e7b574147f2e28e96f520b6e0ecd
0lex1y <kotov.oleksiy@icloud.com>
2025-09-09 03:22:49 +0300

    Fix __init__

--- app/main.py
+++ app/main.py
@@ -37,10 +37,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
