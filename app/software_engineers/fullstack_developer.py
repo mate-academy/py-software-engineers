75dc51717037281a4424fdcd56cf75dd90f6c246
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-30 14:45:04 +0200

    fixing MRO bug in fullstack_developer.py

--- app/software_engineers/fullstack_developer.py
+++ app/software_engineers/fullstack_developer.py
@@ -1,8 +1,12 @@
 from app.software_engineers.backend_developer import BackendDeveloper
 from app.software_engineers.frontend_developer import FrontendDeveloper
+from app.software_engineers.software_engineer import SoftwareEngineer
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
