47436ea7f4057b491274fa0439016db9d064c3f4
Rodion <r.demianetss@gmail.com>
2025-10-18 22:29:34 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -42,11 +42,9 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
-        frontend_skills = ["JavaScript", "HTML", "CSS"]
-        for skill in frontend_skills:
-            if skill not in self.skills:
-                self.skills.append(skill)
+        SoftwareEngineer.__init__(self, name)
+        self.skills.extend(["Python", "SQL", "Django"])
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
