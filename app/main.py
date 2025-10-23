510c69b2c33becf9fed651a760274077023f4632
tomech <109527747+tomech4@users.noreply.github.com>
2025-10-24 00:12:41 +0200

    create FullStackDeveloper class

--- app/main.py
+++ app/main.py
@@ -36,3 +36,12 @@ class AndroidDeveloper(SoftwareEngineer):
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
+
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
