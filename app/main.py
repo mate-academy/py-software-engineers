5cd04cbd49fe1c0c5b8dab3725f4f2902af379f8
Zanko Yaroslav <arikzanko9@gmail.com>
2025-10-30 09:30:57 +0200

    task

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ class FullStackDeveloper(
         super().__init__(name)
         self.skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
 
-    def create_web_application(self) -> None | str:
+    def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
