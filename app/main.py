d432b33994dca1c5dee02ae0bddca2aeba7709a2
Vladislav <vladjigalo07@gmail.com>
2025-11-02 01:16:35 +0200

    develop completed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,54 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.name = name
+        self.skills = []
+
+    def learn_skill(self, skill: str) -> None:
+        if skill not in self.skills and isinstance(skill, str):
+            self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, **kwargs) -> None:
+        super().__init__(name=name, **kwargs)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
+        self.skills = list(dict.fromkeys(self.skills))
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, **kwargs) -> None:
+        super().__init__(name=name, **kwargs)
+        self.skills.extend(["Python", "SQL", "Django"])
+        self.skills = list(dict.fromkeys(self.skills))
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, **kwargs) -> None:
+        super().__init__(name=name, **kwargs)
+        self.skills.extend(["Java", "Android studio"])
+        self.skills = list(dict.fromkeys(self.skills))
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        super().__init__(name=name)
+        self.skills = list(set(self.skills))
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
+        return None
