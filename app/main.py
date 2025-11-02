880c87941e25d63e04c7bf98259897d02321905e
Nikita <hkorobko@gmail.com>
2025-11-02 15:33:07 +0200

    Fixed Py Software Engineers

--- app/main.py
+++ app/main.py
@@ -38,6 +38,9 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(self, name: str) -> None:
+        BackendDeveloper.__init__(self, name)
+        FrontendDeveloper.__init__(self, name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
