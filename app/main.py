07fdb0037700f9f6aab8a72049cc243cbfb71d4c
Nikita Grinyov <nikita3grynov@gmail.com>
2025-11-03 15:36:13 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -42,6 +42,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
-        print(f"{self.name} is creating a web application...")
+        print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
