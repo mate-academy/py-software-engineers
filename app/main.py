59e9a325af4c180ee2d1d7fd8f4ec3087018aec0
Vladyslav Stasiuk <cheshire.cat.universe@gmail.com>
2025-11-05 01:49:25 +0200

    changed order of parent classes in fullstackdeveloper class

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         super().create_powerful_api()
