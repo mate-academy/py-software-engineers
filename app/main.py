f6baf2e798b0fa09a91ab4a4e3b1cbb13e358515
Syrota Oleksii <OleksiiSyrota@gmail.com>
2025-11-06 23:40:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -38,16 +38,9 @@ class AndroidDeveloper(SoftwareEngineer):
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
 
-    skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
+    skills = BackendDeveloper.skills + FrontendDeveloper.skills
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
-
-# new_skills = ["one", "two"]
-# engieenier = BackendDeveloper("Max")
-# print(engieenier.skills)
-# engieenier.learn_skill(new_skills)
-# print(engieenier.skills)
