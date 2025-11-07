f20137bd03365ab114a07c8059676c8974f4afb2
Artem <artemp2208@gmail.com>
2025-11-07 09:16:41 +0200

    del init

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-
 class SoftwareEngineer:
     def __init__(self, name: str) -> None:
         self.name = name
@@ -38,13 +37,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        for skill in ["JavaScript", "HTML", "CSS"]:
-            if skill not in self.skills:
-                self.skills.append(skill)
-
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
