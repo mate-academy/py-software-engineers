31f6ed2a093bc93a06dff90177a70c0f89563f33
Vladislav <vladjigalo07@gmail.com>
2025-11-07 17:54:44 +0200

    develop completed v4

--- app/main.py
+++ app/main.py
@@ -42,10 +42,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name=name)
-
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
