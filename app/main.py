4cfc4aefa55742db1caee2ff3676643b662003dd
Yura Valko <yuravalko166@gmail.com>
2025-11-09 11:42:23 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -48,3 +48,31 @@ class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
+
+engineer = SoftwareEngineer("Max")
+print(engineer.skills)          # []
+engineer.learn_skill("Python")
+print(engineer.skills)          # ["Python"]
+
+front_dev = FrontendDeveloper("Alisa")
+print(front_dev.skills)         # ["JavaScript", "HTML", "CSS"]
+page = front_dev.create_awesome_web_page()
+print(page)                     # "<h1>Hello world</h1>"
+
+backend_dev = BackendDeveloper("Bob")
+print(backend_dev.skills)       # ["Python", "SQL", "Django"]
+address = backend_dev.create_powerful_api()
+print(address)                  # "http://127.0.0.1:8000"
+
+android_dev = AndroidDeveloper("Beth")
+print(android_dev.skills)       # ["Java", "Android studio"]
+app = android_dev.create_smooth_mobile_app()
+print(app)                      # "Ads every three swipes"
+
+full_stack_dev = FullStackDeveloper("Tom")
+print(full_stack_dev.skills)
+# ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]
+full_stack_dev.create_web_application()
+# Tom started creating a web application...
+# Tom is creating an API...
+# Tom is creating a webpage...
