2dcc709b2308df79f36c1b76336f68f3f2f55e3f
Oleksandr Tkach <shvun379@gmail.com>
2025-12-08 15:58:17 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills.extend(["JavaScript", "HTML", "CSS"])
+        self.skills.extend(["JavaScript", "CSS", "HTML"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -40,13 +40,6 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        SoftwareEngineer.__init__(self, name)
-        self.skills.extend(["Python",
-                            "SQL",
-                            "Django",
-                            "JavaScript",
-                            "CSS",
-                            "HTML"])
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
