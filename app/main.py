b03a8b838551d31a2f250e1eb89d012bdef4abf5
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2025-12-09 21:02:11 +0000

    updated MRO

--- app/main.py
+++ app/main.py
@@ -37,11 +37,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(
-    BackendDeveloper,
-    FrontendDeveloper,
-    SoftwareEngineer
-):
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
