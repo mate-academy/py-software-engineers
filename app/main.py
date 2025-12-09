e42ca83d4f34c9c8e7b88642bfb9201756c5b4ad
Artem <artem90078@gmail.com>
2025-12-09 19:55:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,12 +7,10 @@ class SoftwareEngineer:
         self.skills.append(skill)
 
 
-class FrontEnd(SoftwareEngineer):
+class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.learn_skill("JavaScript")
-        self.learn_skill("HTML")
-        self.learn_skill("CSS")
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         message = f"{self.name} is creating a webpage..."
@@ -20,12 +18,10 @@ class FrontEnd(SoftwareEngineer):
         return "<h1>Hello world</h1>"
 
 
-class BackEnd(SoftwareEngineer):
+class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.learn_skill("Python")
-        self.learn_skill("SQL")
-        self.learn_skill("Django")
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         message = f"{self.name} is creating an API..."
@@ -36,8 +32,7 @@ class BackEnd(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.learn_skill("Java")
-        self.learn_skill("Android studio")
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         message = f"{self.name} is creating a mobile app..."
@@ -45,10 +40,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(FrontEnd, BackEnd):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
