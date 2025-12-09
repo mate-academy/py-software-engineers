aca75ef73c3c1f4b8f479d63c2d56786362404ef
Ana Karolina <anakarolina538@gmail.com>
2025-12-09 11:20:18 -0300

    solving challenge

--- app/main.py
+++ app/main.py
@@ -38,16 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        seen = set()
-        unique_skills = []
-        for skill in self.skills:
-            if skill not in seen:
-                unique_skills.append(skill)
-                seen.add(skill)
-        self.skills = unique_skills
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
