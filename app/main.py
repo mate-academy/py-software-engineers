566b8f254362d0a2afc72a13c76c18beee19a49d
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2025-12-17 15:07:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
