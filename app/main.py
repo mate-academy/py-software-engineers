e02e5b57752e20316889272e6841f3f0a780697c
Johan Rangel <johan.apr99@gmail.com>
2025-12-31 15:30:26 +0000

    new developer

--- app/main.py
+++ app/main.py
@@ -38,6 +38,6 @@ class FullStackDeveloper(FrontendDeveloper, BackendDeveloper, AndroidDeveloper):
         self.name = name
 
     def create_web_application(self) -> None:
+        print (f"{self.name} started creating a web application...")
         self.create_awesome_web_page()
         self.create_powerful_api()
-        self.create_smooth_mobile_app()
