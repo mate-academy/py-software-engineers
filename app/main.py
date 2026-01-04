42221fe1531430397bf67d8cf972e8e50ae0364b
Yurii Medvid <medvedimedved16@gmail.com>
2026-01-04 17:30:15 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,47 @@
-# write your code here
+class SoftwareEngineer:
+    DEFAULT_SKILLS = []
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.skills = []
+        for new_skill in self.DEFAULT_SKILLS:
+            self.learn_skill(new_skill)
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    DEFAULT_SKILLS = ["JavaScript", "HTML", "CSS"]
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    DEFAULT_SKILLS = ["Python", "SQL", "Django"]
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    DEFAULT_SKILLS = ["Java", "Android studio"]
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        for default_skil in FrontendDeveloper.DEFAULT_SKILLS:
+            self.learn_skill(default_skil)
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
