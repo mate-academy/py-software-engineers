8de494ad36a07be990af30a78df5e1404456aa2b
Ambra <trank13f@gmail.com>
2026-01-11 10:59:52 +0100

    created SoftwareEngineer class

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-# write your code here
+class SoftwareEngineer:
+    skills = []
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def learn_skills(self, skill: str) -> None:
+        self.skills.append(skill)
