1d88173062f9a1b2ce9fcf5b803ae8021cbfb6ec
Ambra <trank13f@gmail.com>
2026-01-11 11:05:09 +0100

    created Frontend class, add some skills by default and one metheds that return a web page code

--- app/main.py
+++ app/main.py
@@ -6,3 +6,14 @@ class SoftwareEngineer:
 
     def learn_skills(self, skill: str) -> None:
         self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    skills = ["JavaScript", "HTML", "CSS"]
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
