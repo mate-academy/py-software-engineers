f53d64c7ee907dad856b10141ec1be7855368489
Dmytro <ys33ys33ys55@gmail.com>
2026-01-12 20:30:32 +0100

    Solution fixed

--- app/main.py
+++ app/main.py
@@ -6,12 +6,15 @@ class SoftwareEngineer:
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
+    def learn_many_skills(self, *skills_list) -> None:
+        for skill in skills_list:
+            self.learn_skill(skill)
+
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        for skill in "JavaScript", "HTML", "CSS":
-            self.learn_skill(skill)
+        self.learn_many_skills("JavaScript", "HTML", "CSS")
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -21,8 +24,7 @@ class FrontendDeveloper(SoftwareEngineer):
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        for skill in "Python", "SQL", "Django":
-            self.learn_skill(skill)
+        self.learn_many_skills("Python", "SQL", "Django")
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -32,8 +34,7 @@ class BackendDeveloper(SoftwareEngineer):
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        for skill in "Java", "Android studio":
-            self.learn_skill(skill)
+        self.learn_many_skills("Java", "Android studio")
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
