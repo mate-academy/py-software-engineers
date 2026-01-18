35d0883f0b4d19b7718894ca488ca9ad1ce3583a
titoiriepal <titoiriepal@hotmail.com>
2026-01-18 20:04:21 +0100

    Classes SoftwareEngineer and FrontendDeveloper created

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str, skills: list | None = None) -> None:
+        self.name = name
+        if skills is None:
+            self.skills = []
+        else:
+            self.skills = skills
+
+    def learn_skill(self, skill: str) -> None:
+        if skill not in self.skills:
+            self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, ["Javascript", "HTML", "CSS"])
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world!</h1>"
+
+
+engineer = SoftwareEngineer("Max")
+# engineer.skills == []
+engineer.learn_skill("Python")
+print(engineer.skills)
