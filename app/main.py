730e55cd51f70deb3c567b308c943e02c6537866
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-19 12:16:06 +0100

    fixed very last error

--- app/main.py
+++ app/main.py
@@ -45,7 +45,3 @@ class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
         self.create_awesome_web_page()
-
-mike = FullStackDeveloper("Mike")
-
-print(mike.skills)
