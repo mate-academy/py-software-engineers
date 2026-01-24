823cb108745bb6632eb733410d5b3187022233ca
Lucas Adrian Sebastian Casas <lucas.a.s.casas@outlook.es>
2026-01-24 15:37:33 -0300

    feat: implement FrontendDeveloper class with default skills and webpage creation.

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ class SoftwareEngineer:
             name: str
     ) -> None:
         """
-        Initializes the engineer's name and manages the skills he
+        Initializes the engineer's name and manages the skills they
         can learn.
 
         Attributes:
@@ -30,3 +30,35 @@ class SoftwareEngineer:
             new_skill (str): the skill learned.
         """
         self.skills.append(new_skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    """
+    It represents a frontend developer who can create web pages.
+    """
+
+    def __init__(
+        self,
+        name: str
+    ) -> None:
+        """
+        Initializes the developer's name and manages the skills they learned.
+
+        Attributes:
+            name (str): Developer's name.
+            skills (list[str]): Skills learned.
+        """
+        super().__init__(name)
+        self.skills += ["JavaScript", "HTML", "CSS"]
+
+    def create_awesome_web_page(
+        self
+    ) -> str:
+        """
+        Displays a message in the console and returns the webpage HTML.
+
+        Returns:
+            str: The HTML code of the created webpage.
+        """
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
