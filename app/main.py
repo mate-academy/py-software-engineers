7bcd3038557e80a664a0bd35466c1427cd2def50
Krzysztof Hajlasz <krhajlas@cisco.com>
2026-02-08 13:46:30 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,56 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str, skills=None) -> None:  # noqa: ANN001
+        self.name = name
+        self.skills = skills if skills is not None else []
+
+    def learn_skill(self, skill: str) -> None:
+        if skill not in self.skills:
+            self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills=None) -> None:  # noqa: ANN001
+        super().__init__(name, skills)
+        if self.skills is None:
+            self.skills = []
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills=None) -> None:  # noqa: ANN001
+        super().__init__(name, skills)
+        if self.skills is None:
+            self.skills = []
+        self.skills.extend(["Python", "Django", "SQL"])
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str, skills=None) -> None:  # noqa: ANN001
+        super().__init__(name, skills)
+        if self.skills is None:
+            self.skills = []
+        self.skills.extend(["Java", "Android studio"])
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(self, name: str, skills=None) -> None:  # noqa: ANN001
+        super().__init__(name, skills)
+        if self.skills is None:
+            self.skills = []
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
