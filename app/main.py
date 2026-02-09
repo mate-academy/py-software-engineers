5fd7893831a43312def503c266f145dee0cfd879
Jerzy Joniec <j.joniec5@gmail.com>
2026-02-09 12:58:03 +0100

    „final”

--- app/main.py
+++ app/main.py
@@ -10,11 +10,8 @@ class SoftwareEngineer:
 
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.skills = []
         super().__init__(name)
-        self.skills.append("JavaScript")
-        self.skills.append("HTML")
-        self.skills.append("CSS")
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
@@ -23,11 +20,8 @@ class FrontendDeveloper(SoftwareEngineer):
 
 class BackendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.skills = []
         super().__init__(name)
-        self.skills.append("Python")
-        self.skills.append("SQL")
-        self.skills.append("Django")
+        self.skills.extend(["Python", "SQL", "Django"])
 
     def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
@@ -36,10 +30,8 @@ class BackendDeveloper(SoftwareEngineer):
 
 class AndroidDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
-        self.skills = []
         super().__init__(name)
-        self.skills.append("Java")
-        self.skills.append("Android studio")
+        self.skills.extend(["Java", "Android studio"])
 
     def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
